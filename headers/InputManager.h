09cbb0a297818de9c2513933756b78feda4a1978
theoldestsc <ferrarigto733@gmail.com>
2021-08-14 23:37:43 +0300

    Mouse Events

--- headers/InputManager.h
+++ headers/InputManager.h
@@ -5,6 +5,17 @@
 #include <memory>
 #include "Game.h"
 #include <SDL.h>
+#include "Vector2D.h"
+#include <vector>
+
+//TODO: FIX All enums, use EnumClass
+enum mouse_buttons
+{
+    LEFT_MB = 0,
+    MIDDLE_MB = 1,
+    RIGHT_MB = 2
+};
+    
 
 class InputManager
 {
@@ -13,10 +24,16 @@ class InputManager
         void Update();
         void Clean();
         bool isKeyDown(SDL_Scancode key);
+        Vector2D* getMousePosition();
+        bool getMouseButtonState(int buttonNumber);
         ~InputManager();
     private:
+        InputManager(const InputManager&) = delete;
+        InputManager& operator=(const InputManager&) = delete;
         InputManager();
         Uint8* state;
+        Vector2D* m_mousePosition;
+        std::vector<bool> m_mouseButtonStates;
         static std::unique_ptr<InputManager> inputManagerInstance;
 };
 #endif
--- src/InputManager.cpp
+++ src/InputManager.cpp
@@ -4,8 +4,25 @@ std::unique_ptr<InputManager> InputManager::inputManagerInstance = nullptr;
 
 InputManager::InputManager()
 {
+    m_mousePosition = new Vector2D(0, 0);
+    for(int i = 0; i < 3; i++)
+    {
+        m_mouseButtonStates.push_back(false);
+    }
 
 }
+
+bool InputManager::getMouseButtonState(int buttonNumber)
+{
+    return m_mouseButtonStates[buttonNumber];
+}
+
+
+Vector2D* InputManager::getMousePosition()
+{
+    return m_mousePosition;
+}
+
 
 std::unique_ptr<InputManager>& InputManager::Instance()
 {
@@ -26,14 +43,48 @@ void InputManager::Update()
         case SDL_QUIT:
             Game::Instance()->Quit();
             break;
-        case SDL_KEYUP:
-            
+        case SDL_MOUSEMOTION:
+            m_mousePosition->setX(event.motion.x);
+            m_mousePosition->setY(event.motion.y);
+        case SDL_MOUSEBUTTONDOWN:
+            switch (event.button.button)
+            {
+            case SDL_BUTTON_LEFT:
+                m_mouseButtonStates[LEFT_MB] = true;
+                break;
+            case SDL_BUTTON_MIDDLE:
+                m_mouseButtonStates[MIDDLE_MB] = true;
+                break;
+            case SDL_BUTTON_RIGHT:
+                m_mouseButtonStates[RIGHT_MB] = true;
+                break;
+            default:
+                break;
+            }
+            break;
+        case SDL_MOUSEBUTTONUP:
+            switch (event.button.button)
+            {
+            case SDL_BUTTON_LEFT:
+                m_mouseButtonStates[LEFT_MB] = false;
+                break;
+            case SDL_BUTTON_MIDDLE:
+                m_mouseButtonStates[MIDDLE_MB] = false;
+                break;
+            case SDL_BUTTON_RIGHT:
+                m_mouseButtonStates[RIGHT_MB] = false;
+                break;
+
+            default:
+                break;
+            }
             break;
         default:
             break;
         }
     }
-    state = (Uint8*)SDL_GetKeyboardState(NULL);
+  
+    state = const_cast<Uint8*>(SDL_GetKeyboardState(NULL));
 }
 
 
@@ -61,5 +112,5 @@ void InputManager::Clean()
 
 InputManager::~InputManager()
 {
-
+    delete m_mousePosition;
 }
